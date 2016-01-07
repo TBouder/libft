8f182aed2e9755a9a558d29e2886363cd0267585
Tbouder-mac <tbouder@student.42.fr>
2016-01-07 22:26:51 +0100

    uplibft

--- ft_realloc.c
+++ ft_realloc.c
@@ -6,7 +6,7 @@
 /*   By: tbouder <tbouder@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/08 15:21:57 by tbouder           #+#    #+#             */
-/*   Updated: 2016/01/04 11:36:52 by tbouder          ###   ########.fr       */
+/*   Updated: 2016/01/05 17:40:52 by tbouder          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,30 @@
 ** is identical to a call to malloc() for size bytes.
 */
 
+#include <stdlib.h>
 #include "libft.h"
 
 void	*ft_realloc(void *ptr, size_t size)
 {
-	void	*temp;
+	void		*new_mem;
+	size_t		len;
+	t_block		*block;
 
-	temp = ft_memalloc(size);
-	if (ptr)
+	if (size == 0)
 	{
-		if (temp)
-			ft_memcpy(temp, ptr, size);
-		ft_memdel(&ptr);
+		free(ptr);
+		return (NULL);
 	}
-	return (temp);
+	if (ptr == NULL)
+		return (malloc(size));
+	block = ptr - 8;
+	if (size > block->size / 2 && size < block->size - 8)
+		return (ptr);
+	new_mem = ft_memalloc(size);
+	len = block->size - 8;
+	if (len > size)
+		len = size;
+	ft_memcpy(new_mem, ptr, len);
+	free(ptr);
+	return (new_mem);
 }
