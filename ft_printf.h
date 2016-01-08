d7f0c63b6069438fa038fe99e164b76ba2326c82
Thomas BOUDER <tbouder@e3r8p18.42.fr>
2016-01-08 16:04:38 +0100

    First commit

---
+++ ft_printf.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tbouder <tbouder@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/01/08 15:59:36 by tbouder           #+#    #+#             */
+/*   Updated: 2016/01/08 16:02:28 by tbouder          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+int		ft_printf(const char * restrict format, ...);
+
+#endif
