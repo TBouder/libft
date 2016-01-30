5fa821f83ed950092bf809bea74594e7dbd2da0a
Thomas BOUDER <tbouder@e3r11p23.42.fr>
2016-01-30 04:21:55 +0100

    corr

--- ft_nb/ft_nbrnew.c
+++ ft_nb/ft_nbrnew.c
@@ -6,9 +6,16 @@
 /*   By: tbouder <tbouder@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/30 04:18:19 by tbouder           #+#    #+#             */
-/*   Updated: 2016/01/30 04:18:28 by tbouder          ###   ########.fr       */
+/*   Updated: 2016/01/30 04:20:40 by tbouder          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/*
+** The ft_nbrnew() function malloc an int buffer and initialize it with 0.
+*/
+
+#include "../libft.h"
+#include <stdlib.h>
 
 int		*ft_nbrnew(size_t size)
 {
