1c061f8aa49484cca54a06f4cf116a1769d98ea3
Thomas Bouder <Tbouder@iMac-de-Thomas.local>
2016-02-04 18:58:59 +0100

    Ajout de mise a 0 de content_size pour clear

--- ft_lst/ft_lstclr.c
+++ ft_lst/ft_lstclr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstclr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tbouder <tbouder@student.42.fr>            +#+  +:+       +#+        */
+/*   By: Tbouder <Tbouder@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/22 12:09:59 by tbouder           #+#    #+#             */
-/*   Updated: 2016/01/08 15:17:20 by tbouder          ###   ########.fr       */
+/*   Updated: 2016/02/04 16:42:46 by Tbouder          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,7 @@ void		ft_lstclr(t_list **begin_list)
 		{
 			temp = free_list;
 			free_list = free_list->next;
+			temp->content_size = 0;
 			free(temp->content);
 			free(temp);
 		}
