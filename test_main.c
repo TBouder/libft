2c8a91fd0edda88dc681848405cf56cf78117bfe
tbouder <tbouder@student.42.fr>
2016-04-01 15:00:30 +0200

    Auto-Update 15:00:30, Friday 01 April

--- test_main.c
+++ test_main.c
@@ -6,7 +6,7 @@
 /*   By: tbouder <tbouder@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/14 18:34:22 by tbouder           #+#    #+#             */
-/*   Updated: 2016/04/01 14:45:13 by tbouder          ###   ########.fr       */
+/*   Updated: 2016/04/01 14:56:40 by tbouder          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,9 @@ int		main()
 	int	i;
 	// int	j;
 
-	ft_putchar(231);
-	ft_putchar(177);
-	ft_putchar(178);
+	// ft_putchar(231);
+	// ft_putchar(177);
+	// ft_putchar(178);
 
 	i = ft_printf("Bonjour, %C, %d\n", L'米', 42);
 	// ft_printf("\n\n\n");
